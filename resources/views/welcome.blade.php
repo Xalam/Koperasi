676fe4269b4f644ea8e558b1f00f9cce3af321b7
Maulana Imam Tantowi <maulanait007@gmail.com>
2021-05-16 13:02:33 +0700

    Edit layout Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,8 +55,8 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
-                                    <h1>Testing</h1>
+                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end. Ya gimana lagi....
+                                    <h1>Cobaan</h1>
                                 </div>
                             </div>
                         </div>
