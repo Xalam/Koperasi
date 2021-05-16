bfde084d758829ec271bef8a8e83673c3a200243
xalam <ganufvyandana@gmail.com>
2021-05-16 13:09:20 +0700

    edit welcome lagi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end. Ya gimana lagi....
-                                    <h1>Cobaan</h1>
+                                    <h1>Cobaan berat apalagi</h1>
                                 </div>
                             </div>
                         </div>
